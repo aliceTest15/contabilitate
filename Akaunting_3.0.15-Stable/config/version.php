077ab6ece549c2976790828b0dff64c0aaeeafef
LENOVO_X1CARBON\User <sarcaalina1993@gmail.com>
2023-06-04 22:53:25 +0300

    initial commit.

---
+++ Akaunting_3.0.15-Stable/config/version.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name'      =>  'Akaunting',
+
+    'code'      =>  'Tailwind',
+
+    'major'     =>  '3',
+
+    'minor'     =>  '0',
+
+    'patch'     =>  '15',
+
+    'build'     =>  '',
+
+    'status'    =>  'Stable',
+
+    'date'      =>  '31-May-2023',
+
+    'time'      =>  '17:00',
+
+    'zone'      =>  'GMT +3',
+
+];
